63fb89ea4ef0d830990611ce021efbbedc902fa3
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-24 00:57:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,7 +31,7 @@ attr_accessor :name, :songs
     self.all.find { |x| x.name == name}
   end
 
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
 
