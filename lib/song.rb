261e96dcf3e918219189043c612957456edcd005
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-24 00:32:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
 
-attr_accessor :name
+attr_accessor :title
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
   end
 
   def self.new_by_filename(filename)
