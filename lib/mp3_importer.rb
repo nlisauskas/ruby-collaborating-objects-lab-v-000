487b422af3244bef057d4db76574cb45d66f4dc2
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-24 00:28:00 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,10 +4,13 @@ class MP3Importer
     @path = path
   end
 
-  def import
+  def import(files)
+    files.each do |file|
+    Song.new_by_filename(file)
+  end
   end
 
   def files
   end
-  
+
 end
